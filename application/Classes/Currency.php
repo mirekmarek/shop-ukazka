881faca523b0d518815e3aafde50a46f11bea670
mirekmarek <mirek.marek.2m@gmail.com>
2023-03-21 16:54:24 +0100

    Cena - zobrazeni

--- application/Classes/Currency.php
+++ application/Classes/Currency.php
@@ -10,4 +10,16 @@ class Currency
 	{
 		return Application_Shop::getBase()->getLocalizedData($locale)->getParameter('currency');
 	}
+	
+	
+	public static function format( float $price, ?Locale $locale=null ) : string
+	{
+		if(!$locale) {
+			$locale = Locale::getCurrentLocale();
+		}
+		
+		$formatter = new \NumberFormatter( $locale->toString(), \NumberFormatter::CURRENCY );
+		
+		return $formatter->formatCurrency( $price, static::getCurrencyCode( $locale ) );
+	}
 }
--- application/dictionaries/sk_SK/Catalog.Browser.php
+++ application/dictionaries/sk_SK/Catalog.Browser.php
@@ -1,4 +1,5 @@
 <?php
 return [
 	'Price:' => '',
+	'Konečná cena: %price%' => '',
 ];
