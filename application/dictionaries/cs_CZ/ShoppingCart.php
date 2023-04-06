3a6f146277e2264b02830ad6f06b9ed4ba0aff43
mirekmarek <mirek.marek.2m@gmail.com>
2023-04-06 11:07:12 +0200

    Doladeni detailu kosiku

--- application/dictionaries/cs_CZ/ShoppingCart.php
+++ application/dictionaries/cs_CZ/ShoppingCart.php
@@ -1,6 +1,5 @@
 <?php
 return [
-	'Do košíku přidáno' => '',
 	'Položka přidána' => '',
 	'Objednat' => '',
 	'Pokračovat v objednávce' => '',
--- application/dictionaries/sk_SK/ShoppingCart.php
+++ application/dictionaries/sk_SK/ShoppingCart.php
@@ -1,7 +1,4 @@
 <?php
 return [
-	'Položka pridaná' => '',
 	'Položka přidána' => '',
-	'Objednat' => '',
-	'Pokračovat v objednávce' => 'Pokračovať v objednávaní',
 ];
